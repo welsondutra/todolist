eb0d2c253783152e859cf65b7efec75ee8915db5
Welson Dutra <welsonpereira83@gmail.com>
2023-07-06 13:03:00 -0300

    primeiro commit

---
+++ js/scrips.js
@@ -0,0 +1,23 @@
+
+// CRUD
+// Esse é um crud para uma lista de tarefas 
+
+//Create
+function crateTask({title, description, status}) {
+
+}
+
+//Read
+function readTask({id}) {
+
+}
+
+//Update
+function updateTask({id,title, description,status}) {
+
+}
+
+//Delete
+function deleteTask({id}) {
+
+}
